6ae951c5547811731442b6cd82c02b5dc98fadd5
Thazniya <thasniyakader@gmail.com>
2018-03-24 11:41:19 +0530

    server program modified

--- UDPServer.java
+++ UDPServer.java
@@ -8,3 +8,7 @@ public static void main(String args[]) throws Exception
       DatagramSocket serverSocket = new DatagramSocket(9876);
             byte[] receiveData = new byte[1024];
             byte[] sendData = new byte[1024];
+            while(true)
+               {
+                  DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
+                  serverSocket.receive(receivePacket);
