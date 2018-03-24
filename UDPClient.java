ad3a886810ce31d7f12d2c34f4c8d2ce0185b4fc
Tina <tinachemmannur10@gmail.com>
2018-03-24 11:44:20 +0530

    updating client

--- UDPClient.java
+++ UDPClient.java
@@ -1,3 +1,8 @@
 import java.io.*;
 import java.net.*;
 class UDPClient
+{
+   public static void main(String args[]) throws Exception
+   {
+      BufferedReader inFromUser =
+         new BufferedReader(new InputStreamReader(System.in));
