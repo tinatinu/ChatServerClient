e20d0fd612b5638b6a18a3f7edef994e534d5ad0
Tina <tinachemmannur10@gmail.com>
2018-03-24 11:45:09 +0530

    second commit

--- UDPClient.java
+++ UDPClient.java
@@ -5,4 +5,8 @@ class UDPClient
    public static void main(String args[]) throws Exception
    {
       BufferedReader inFromUser =
-         new BufferedReader(new InputStreamReader(System.in));
+      new BufferedReader(new InputStreamReader(System.in));
+      DatagramSocket clientSocket = new DatagramSocket();
+      InetAddress IPAddress = InetAddress.getByName("localhost");
+      byte[] sendData = new byte[1024];
+      byte[] receiveData = new byte[1024];
