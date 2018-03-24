b1343f119442cb0f922124b7c70af36dbce11fad
Thazniya <thasniyakader@gmail.com>
2018-03-24 11:43:26 +0530

    mesage error corrected

--- UDPServer.java
+++ UDPServer.java
@@ -13,7 +13,7 @@ public static void main(String args[]) throws Exception
                   DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
                   serverSocket.receive(receivePacket);
                   String sentence = new String( receivePacket.getData());
-                  System.out.println("RECEIVED   : " + sentence);
+                  System.out.println("RECEIVED: " + sentence);
                   InetAddress IPAddress = receivePacket.getAddress();
                   int port = receivePacket.getPort();
                   String capitalizedSentence = sentence.toUpperCase();
